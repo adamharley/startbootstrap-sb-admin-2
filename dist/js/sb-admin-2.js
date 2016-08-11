9cc2adfce0b81217105e563a69785291a490e9aa
Adam Harley <github@adamharley.co.uk>
2016-08-11 11:45:47 +0100

    window, screen and location are global objects

--- dist/js/sb-admin-2.js
+++ dist/js/sb-admin-2.js
@@ -6,7 +6,7 @@ $(function() {
     // Sets the min-height of #page-wrapper to window size
     $(window).bind("load resize", function() {
         var topOffset = 50;
-        var width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
+        var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
         if (width < 768) {
             $('div.navbar-collapse').addClass('collapse');
             topOffset = 100; // 2-row-menu
@@ -14,7 +14,7 @@ $(function() {
             $('div.navbar-collapse').removeClass('collapse');
         }
 
-        var height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
+        var height = ((window.innerHeight > 0) ? window.innerHeight : screen.height) - 1;
         height = height - topOffset;
         if (height < 1) {
             height = 1;
@@ -24,7 +24,7 @@ $(function() {
     });
 
     var element = $('ul.nav a').filter(function() {
-        return this.href == window.location;
+        return this.href == location;
     }).addClass('active').parent();
 
     while (element.is('li')) {
