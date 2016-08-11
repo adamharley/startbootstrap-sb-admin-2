cc30479c332828f3ae26866aa25305359ed182c0
Adam Harley <github@adamharley.co.uk>
2016-08-11 11:34:01 +0100

    simplify while() loop

--- dist/js/sb-admin-2.js
+++ dist/js/sb-admin-2.js
@@ -30,11 +30,7 @@ $(function() {
      return this.href == url;
     }).addClass('active').parent();
 
-    while(true){
-        if (element.is('li')){
-            element = element.parent().addClass('in').parent();
-        } else {
-            break;
-        }
+    while (element.is('li')) {
+        element = element.parent().addClass('in').parent();
     }
 });
